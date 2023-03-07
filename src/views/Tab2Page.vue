9f42c0009bc85b9ef7471a580fba2d0e1aa593de
Joel Christensen <joel@yourmaidenvoyage.com>
2023-03-07 10:05:10 -0700

    Initial camera setup

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,22 +2,73 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Photo Gallery</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
 
-      <ExploreContainer name="Tab 2 page" />
+  <ion-content>
+    <ion-grid>
+      <ion-row>
+        <ion-col size="6" :key="photo" v-for="photo in photos">
+          <ion-img :src="photo.webviewPath"></ion-img>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
+    <!-- <ion-fab> markup  -->
+  </ion-content>
+
+    <ion-content :fullscreen="true">
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <ion-fab-button @click="takePhoto()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script lang="ts">
+  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+  import { camera, trash, close } from 'ionicons/icons';
+  import { usePhotoGallery, UserPhoto } from '@/composables/usePhotoGallery';
+  import {
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+  } from '@ionic/vue';
+
+const { photos, takePhoto } = usePhotoGallery();
+
+export default {
+  name: 'Tab2',
+  components: {
+    IonPage,
+    IonHeader,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+  },
+  setup(){
+    const { takePhoto } = usePhotoGallery();
+
+
+    return {
+      photos,
+      takePhoto,
+      camera, trash, close,
+    }
+  }
+};
+
 </script>
